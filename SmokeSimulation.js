e257e4cbd0068a3d2990fc207aaf31975c586f0f
Eric Lanini <elanini@ucsc.edu>
2018-03-21 12:47:24 -0700

    add smoke to end scene

--- SmokeSimulation.js
+++ SmokeSimulation.js
@@ -1,33 +1,24 @@
-var container;
-var fullScreenQuad, plane;
+function smoke_init(renderer) {
+    var container;
+    var fullScreenQuad, plane;
 
-var camera, scene, renderer;
-var options;
-var tick = 0;
-let bufferScene, bufferTexture;
-var terrain_mesh;
-let smoke_mesh;
-var FBO_A, FBO_B;
-function init() {
-
-    let gui = new dat.GUI({width: 300})
-    container = document.getElementById('container');
-    console.log(container)
-    let width = 512
-    let height = 512
+    var camera, scene;
+    var options;
+    var tick = 0;
+    let bufferScene, bufferTexture;
+    var terrain_mesh;
+    let smoke_mesh;
+    var FBO_A, FBO_B;
+    let width = 256
+    let height = 256
     camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
     camera.position.z = 1;
-    // var controls = new THREE.OrbitControls(camera)
-    // camera.position.z = 100;
-    // camera.position.y = 40;
-    // controls.update();
-
     scene = new THREE.Scene();
 
     bufferScene = new THREE.Scene()
     bufferTexture = new THREE.WebGLRenderTarget( 
-        1024, 
-        1024, 
+        256, 
+        256, 
         { 
             minFilter: THREE.LinearFilter, 
             magFilter: THREE.NearestFilter
@@ -41,59 +32,30 @@ function init() {
     smoke_mesh = generate_smoke_mesh(bufferTexture)
     bufferScene.add(smoke_mesh)
 
-    plane = new THREE.PlaneBufferGeometry( 1024, 1024)
-	fullScreenQuad = new THREE.Mesh( plane, new THREE.MeshBasicMaterial() );
+    plane = new THREE.PlaneBufferGeometry( 256, 256)
+    fullScreenQuad = new THREE.Mesh( plane, new THREE.MeshBasicMaterial() );
 	scene.add(fullScreenQuad);
 
-    renderer = new THREE.WebGLRenderer({preserveDrawingBuffer: true});
-    renderer.setClearColor(0x999999);
-    renderer.setSize(1024, 1024);
-    container.appendChild(renderer.domElement);
-
-	FBO_A = new THREE.WebGLRenderTarget( 1024, 1024, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter});
-	FBO_B = new THREE.WebGLRenderTarget( 1024, 1024, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter} );
+	FBO_A = new THREE.WebGLRenderTarget( 256, 256, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter});
+	FBO_B = new THREE.WebGLRenderTarget( 256, 256, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter} );
 
     //make it so that resizing the browser window also resizes the scene
     window.addEventListener('resize', onWindowResize, false);
 
-    animate();
-    // renderk()
-    // var dataURL = renderer.domElement.toDataURL();
-    // window.open(dataURL, "image");
-}
-
-
-
-function animate() {
-
-    requestAnimationFrame(animate);
     render();
+    return fullScreenQuad
+    function render() {
+        requestAnimationFrame(render)
+        tick += 0.01;
+        smoke_mesh.material.uniforms.tick.value = tick;
 
-}
+        renderer.render(bufferScene, camera, FBO_B);
+        fullScreenQuad.material.map = FBO_B.texture;
+        fullScreenQuad.material.transparent = true
 
-function render() {
-    tick += 0.01;
-    smoke_mesh.material.uniforms.tick.value = tick;
-    // terrain_mesh.material.uniforms.fakeShadow.value = options.fakeShadow ? 1 : 0
-
-    //if I want to update the lights, I acutally have to update the material used by each object in the scene. 
-    //material.uniforms.light1_diffuse.value = new THREE.Vector3(0.0,1.0,0.0);
-    renderer.render(bufferScene, camera, FBO_B);
-    fullScreenQuad.material.map = FBO_B.texture;
-
-    renderer.render(scene, camera);
-    var t = FBO_A;
-    FBO_A = FBO_B;
-    FBO_B = t;
-    smoke_mesh.material.uniforms.bufferTexture.value = FBO_A.texture;
-}
-
-
-function onWindowResize(event) {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
+        var t = FBO_A;
+        FBO_A = FBO_B;
+        FBO_B = t;
+        smoke_mesh.material.uniforms.bufferTexture.value = FBO_A.texture;
+    }
 }
--- smoke.js
+++ smoke.js
@@ -13,8 +13,8 @@ varying vec2 vUv;
 uniform sampler2D bufferTexture;
 uniform float tick;
 void main() {
-    float pxStep = 1.0/1024.0;
-    vec2 fragpos = gl_FragCoord.xy/1024.0;
+    float pxStep = 1.0/256.0;
+    vec2 fragpos = gl_FragCoord.xy/256.0;
 	float Np  = texture2D(bufferTexture, vec2(vUv.x + pxStep, vUv.y)).x;
 	float Ep  = texture2D(bufferTexture, vec2(vUv.x, vUv.y + pxStep)).x;
 	float Sp  = texture2D(bufferTexture, vec2(vUv.x - pxStep, vUv.y)).x;
@@ -23,19 +23,19 @@ void main() {
     float timesin = sin(tick*2.0);
     float factor1, factor2, factor;
     // if (timesin < 0.0) {
-        factor1 = 6.0 * 0.016 * ( Np + Ep +  3.0*Sp + 4.0 * Wp -  9.0 * c);
+        factor1 = 6.9 * 0.016 * ( Np + Ep +  3.0*Sp + 4.0 * Wp -  9.0 * c);
     // } else {
-        factor2 = 6.0 * 0.016 * ( 3.0 * Np + Ep +  Sp + 4.0 * Wp -  9.0 * c);
+        factor2 = 6.9 * 0.016 * ( 3.0 * Np + Ep +  Sp + 4.0 * Wp -  9.0 * c);
     // }
     factor = mix(factor1, factor2, ((sin(tick*2.0) / 2.0) + 0.5) / 2.0 + 0.25);
     // factor = clamp(factor, 0.0, 0.01);
     float minimum = 0.003;
     if (factor >= -minimum && factor < 0.0) factor = -minimum;
     c += factor;
-    if (all(lessThan(abs(fragpos - 0.5), vec2(0.002)))) {
+    if (all(lessThan(abs(fragpos - 0.5), vec2(0.01)))) {
         gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
     } else {
-        gl_FragColor = vec4(c, c, c, 1.0);
+        gl_FragColor = vec4(c, c, c, c);
     }
 }
 `
